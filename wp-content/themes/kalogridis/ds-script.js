a24e7f63655a45fdd0d577603d44377d15f92b49
dilutedcoffee <mikaila.munoz@gmail.com>
2022-02-24 14:00:29 -0600

    form test

--- wp-content/themes/kalogridis/ds-script.js
+++ wp-content/themes/kalogridis/ds-script.js
@@ -1,7 +1,8 @@
 // Custom JS goes here ------------
 
 <!-- Adding Netlify to form attributes -->
+<script>
 jQuery(document).ready(function(){
-   jQuery('.et_pb_contact_form clearfix').attr(‘data-netlify’, true);
+   jQuery('.et_pb_contact_form').attr(‘data-netlify’, true);
 });
-
+</script>
