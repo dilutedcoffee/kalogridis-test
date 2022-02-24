61002a09feb37e9fbce069c26dfb6785676215e4
dilutedcoffee <mikaila.munoz@gmail.com>
2022-02-24 13:20:30 -0600

    Form fix

--- wp-content/themes/kalogridis/ds-script.js
+++ wp-content/themes/kalogridis/ds-script.js
@@ -1,1 +1,7 @@
-// Custom JS goes here ------------
+// Custom JS goes here ------------
+
+<!-- Adding Netlify to form attributes -->
+jQuery(document).ready(function(){
+   jQuery('.et_pb_contact_form clearfix').attr(‘data-netlify’, true);
+});
+
